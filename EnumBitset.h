11c5dfa80fccf795c8880a82a8a64ea232690929
Jon White <jxxwhite@gmail.com>
2015-07-08 19:49:12 +0100

    Added EnumBitset.

---
+++ EnumBitset.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Debug.h"
+
+#include <bitset>
+
+namespace Kernel
+{
+	template <typename E>
+	class EnumBitset : public std::bitset<(int)E::_Count>
+	{
+		using Base = std::bitset<(int)E::_Count>;
+	public:
+		EnumBitset() : Base({}) {}
+		EnumBitset(std::initializer_list<E> i)
+		{
+			for (E e : i)
+				set((int)e);
+		}
+
+		bool operator[] (E e) const { return Base::operator[]((int)e); }
+		reference operator[] (E e) { return Base::operator[]((int)e); }
+	};
+}
