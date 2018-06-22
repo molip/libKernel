b4d4302ad553a0468fcde2758334a193553fd053
Jon White <jxxwhite@gmail.com>
2018-06-22 21:49:17 +0100

    ReverseAdapter: prevent unintended conversion when attempting to use with conditional operator

--- Util.h
+++ Util.h
@@ -6,7 +6,7 @@ namespace Kernel
 	class ReverseAdapter
 	{
 	public:
-		ReverseAdapter(const T& t) : m_t(t) {}
+		explicit ReverseAdapter(const T& t) : m_t(t) {}
 
 		typename T::const_reverse_iterator begin() const { return m_t.rbegin(); }
 		typename T::const_reverse_iterator end() const { return m_t.rend(); }
