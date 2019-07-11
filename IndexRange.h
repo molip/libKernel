37206f1f00937493aa1e9799d64c48ed77f51762
Jon White <jxxwhite@gmail.com>
2019-07-11 11:41:12 +0100

    IndexRange reverse

--- IndexRange.h
+++ IndexRange.h
@@ -4,28 +4,38 @@
 
 namespace Kernel
 {
+	template <typename T> class IndexRange;
+
 	template <typename T> class IndexIter
 	{
 	public:
-		IndexIter(std::function<T(size_t)> fnGet, size_t i) : m_index(i), m_fnGet(fnGet) {}
+		IndexIter(const IndexRange<T>& range, size_t i) : m_index(i), m_range(range) {}
 		bool operator !=(const IndexIter& rhs) const { return m_index != rhs.m_index; }
-		T operator* () const { return m_fnGet(m_index); }
+		T operator* () const;
 		void operator++ () { ++m_index; }
 	private:
 		size_t m_index;
-		std::function<T(size_t)> m_fnGet;
+		const IndexRange<T>& m_range;
 	};
  
 	template <typename T> class IndexRange
 	{
 	public:
-		IndexRange(std::function<T(size_t)> fnGet, size_t count) : m_fnGet(fnGet), m_count(count) {}
-		IndexIter<T> begin() const { return IndexIter<T>(m_fnGet, 0); }
-		IndexIter<T> end() const { return IndexIter<T>(m_fnGet, m_count); }
+		IndexRange(std::function<T(size_t)> fnGet, size_t count, bool reverse = false) : m_fnGet(fnGet), m_count(count), m_reverse(reverse) {}
+		IndexIter<T> begin() const { return IndexIter<T>(*this, 0); }
+		IndexIter<T> end() const { return IndexIter<T>(*this, m_count); }
+		T Get(size_t index) const
+		{
+			return m_fnGet(m_reverse ? m_count - 1 - index : index);
+		}
 	private:
 		std::function<T(size_t)> m_fnGet;
 		size_t m_count;
+		bool m_reverse;
 	};
+
+	template <typename T> 
+	T IndexIter<T>::operator* () const { return m_range.Get(m_index); }
 
 	#define KERNEL_DEFINE_INDEXRANGE(rangename, classname, type, getTypeFn, getCountFn) \
 	class rangename : public Kernel::IndexRange<type>\
